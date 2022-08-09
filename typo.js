a36e6e7d4a07c98624fd801505a0bee52c83f275
Abhay Chauhan <110452007+Abhayc13@users.noreply.github.com>
2022-08-09 20:43:50 +0530

    Add files via upload

---
+++ typo.js
@@ -0,0 +1,37 @@
+$('.contents-two a').fancybox({
+    caption : function( instance, item ) {
+    }
+  });
+
+    var a = 0;
+$(window).scroll(function () {
+    var oTop = $(".three-col").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $(".counter").each(function () {
+            var $this = $(this),
+                countTo = $this.attr("data-number");
+            $({
+                countNum: $this.text()
+            }).animate(
+                {
+                    countNum: countTo
+                },
+                {
+                    duration: 850,
+                    easing: "swing",
+                    step: function () {
+                        $this.text(
+                            Math.ceil(this.countNum).toLocaleString("en")
+                        );
+                    },
+                    complete: function () {
+                        $this.text(
+                            Math.ceil(this.countNum).toLocaleString("en")
+                        );
+                    }
+                }
+            );
+        });
+        a = 1;
+    }
+});
